3f3c25b8f1392a81eadc85df091e830f64c7c5a0
mudasar187 <mudasar_cheema@hotmail.com>
2024-07-27 21:09:24 +0200

    peke på riktig main-class i gradle filen

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ tasks {
         enabled = true
         archiveFileName.set("app.jar")
         manifest {
-            attributes["Main-Class"] = "no.nav.sokos.prosjektnavn.ApplicationKt"
+            attributes["Main-Class"] = "no.nav.sokos.mikrofrontendapi.ApplicationKt"
         }
     }
 
